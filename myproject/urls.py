3868d7ba0ba3f87c352a0970df49756aebee86cc
Otavio F. G. Pane <otavio.pane@gmail.com>
2021-11-03 19:44:06 -0300

    Configurado os urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,10 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-# from django.http import HttpResponse
-
-# def index(request):
-#     return HttpResponse('<h1>Django Tutorial</h1>')
+from django.http import HttpResponse
 
 urlpatterns = [
     path('', include('myproject.core.urls'), namespace='core'), 
