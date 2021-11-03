87f60558346c79075f89efa991a60dbc6a147a7e
Otavio F. G. Pane <otavio.pane@gmail.com>
2021-11-03 19:50:02 -0300

    Criado o template index.html

--- myproject/core/views.py
+++ myproject/core/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse('<h1>Django Tutorial</h1>')
 
 def index(request):
     template_name='index.html'
